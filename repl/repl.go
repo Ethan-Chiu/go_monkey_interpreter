7e8dd77e793067ed946da12c1af94f76c7240fa3
Ethan-Chiu <ethanchiu0608@gmail.com>
2025-03-26 18:09:12 +0800

    REPL parse.

--- repl/repl.go
+++ repl/repl.go
@@ -1,30 +1,59 @@
 package repl
 
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "github.com/Ethan-Chiu/go_monkey_interpreter/lexer"
-  "github.com/Ethan-Chiu/go_monkey_interpreter/token"
+	"bufio"
+	"fmt"
+	"github.com/Ethan-Chiu/go_monkey_interpreter/lexer"
+	"github.com/Ethan-Chiu/go_monkey_interpreter/parser"
+	"io"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
-  scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
 
-  for {
-    fmt.Fprintf(out, PROMPT)
-    scanned := scanner.Scan()
-    if !scanned {
-      return
-    }
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
 
-    line := scanner.Text()
-    l := lexer.New(line)
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
 
-    for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-      fmt.Fprintf(out, "%+v\n", tok)
-    }
-  }
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
 }
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
